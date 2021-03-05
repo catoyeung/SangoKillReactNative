7c18cc3d98db03b1d9488921bdb9fe7373dcf1fc
Cato Yeung Pui Kwan (楊沛昆) <cato.yeung@das.kyocera.com>
2021-03-05 17:01:32 +0800

    First Commit on 05/03/2021

---
+++ server/main.js
@@ -0,0 +1,35 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({
+  port: 8080,
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed.
+  }
+});
+
+let onlineUsers = []
+let rooms = []
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(messageInJSON) {
+    console.log('received: %s', JSON.parse(messageInJSON));
+  });
+
+  ws.send('Hello from server');
+});
